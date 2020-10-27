2389776038ba2c6a41b45a837cc83f69512e026d
lida <njulida@outlook.com>
2020-10-27 10:56:51 +0800

    Lab2: trace and sysinfo

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,15 +6,16 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +34,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +45,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,12 +59,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +81,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +97,38 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int trace_mask;
+  if (argint(0, &trace_mask) < 0)
+  {
+    return -1;
+  }
+  myproc()->trace_mask = trace_mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  // printf("enter sysinfo\n");
+  struct sysinfo si; // si means kernel data
+
+  uint64 st; // st means virtual address in user-space
+  if (argaddr(0, &st) < 0)
+    return -1;
+  // printf("obtain virtual address\n");
+  si.freemem = kgetfreemem();
+  si.nproc = getusedproc();
+  // printf("get metadata\n");
+  //copy from kernel to user, from si to st
+  if (copyout(myproc()->pagetable, st, (char *)&si, sizeof(si)) < 0)
+  {
+    return 0xffffffffffffffff;
+  };
+  // printf("copyout done.\n");
+  return 0;
 }
